b728e140b1c618a549d910d15c1c321b66ed03b8
DjTer3a <till.3oov@gmail.com>
2023-05-13 12:47:33 +0300

    refactoring to components

--- src/components/GuessSlots/GuessSlots.js
+++ src/components/GuessSlots/GuessSlots.js
@@ -3,13 +3,15 @@ import { range } from "../../utils";
 import {checkGuess} from "../../../src/game-helpers";
 function GuessSlots({ guess,answer }) {
   const result = checkGuess(guess, answer);
+  function Cell({status, letter}) {
+    const className = status ? `cell ${status}` : 'cell';
+      return <span className={className}>{letter}</span>;
+  }
   return (
     <div className="guess-results">
       <p className="guess">
         {range(5).map((num) => (
-            <span className={result ?  "cell "+result[num].status : "cell"} key={num}>
-            {result ? result[num].letter  : undefined}
-            </span>
+            <Cell key={num} status={result ? result[num].status : undefined} letter={result ? result[num].letter : undefined} />
         ))}
       </p>
     </div>
